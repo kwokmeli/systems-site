6f2df274d7dd1ba3b0d1abf5260ca322e86f1bf3
mlkwok <mkwok@uw.edu>
2017-12-12 13:59:48 -0800

    update theme

--- header.php
+++ header.php
@@ -9,13 +9,15 @@
     <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
     <meta name="description" content="">
     <meta name="author" content="">
+    <div class="header-1"></div><div class="header-2"></div><div class="header-3"></div><div class="header-4"></div>
 <div class="blog-header">
+
  <title> <?php echo get_bloginfo('name'); ?> </title>
       <div class="blog-title"><a href="<?php bloginfo('wpurl');?>"><?php echo get_bloginfo('name');?></a></div>
 
       <div class="search-box"><?php get_search_form(); ?></div>
     <?php wp_head(); ?>
-  </div>
+</div>
   </head>
 
   <body>
--- searchform.php
+++ searchform.php
@@ -1,6 +1,6 @@
 <form role="search" method="get" id="searchform" class="searchform" action="<?php echo home_url( '/' ); ?>">
     <div>
         <input type="text" value="" name="s" id="s" placeholder="Search..." />
-        <div class="search-button"><input type="image" src="<?php bloginfo('template_directory'); ?>/img/mag.png" id="searchsubmit" /></div>
+        <div class="search-button"><input type="image" src="<?php bloginfo('template_directory'); ?>/img/mag-blue.png" id="searchsubmit" /></div>
     </div>
 </form>
