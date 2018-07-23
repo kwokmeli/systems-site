fc1849dd3492429e635cc60230981acb55d14b1e
mlkwok <mkwok@uw.edu>
2018-07-23 12:15:35 -0700

    add function to prevent password protected sites from being listed on sitemap

--- functions.php
+++ functions.php
@@ -8,36 +8,22 @@ function wpb_add_google_fonts() {
 	wp_enqueue_style( 'wpb-google-fonts', 'https://fonts.googleapis.com/css?family=Open+Sans:300,400,600', false );
 }
 
-// function set_post_password_expires() {
-// 	return time() + 10; // Expire in 10 seconds
-// }
+// https://gist.github.com/bonny/5772054
+// Prevent password-protected pages from showing up on the sitemap
+function ep_exclude_password_protected_pages($pages, $r) {
+	if (isset($r["exclude_password_protected"]) && $r["exclude_password_protected"]) {
+		for ($i = 0; $i < sizeof($pages); $i++) {
+			if (post_password_required($pages[$i])) {
+				unset($pages[$i]);
+			}
+		}
+	}
+	return $pages;
+}
 
-// // Hide password-protected posts from Archives, home page, etc.
-// // Filter to hide protected posts
-// function exclude_protected($where) {
-// 	global $wpdb;
-// 	return $where .= " AND {$wpdb->posts}.post_password = '' ";
-// }
-//
-// // Decide where to display them
-// function exclude_protected_action($query) {
-// 	if( !is_single() && !is_page() && !is_admin() ) {
-// 		add_filter( 'posts_where', 'exclude_protected' );
-// 	}
-// }
-//
-// // Action to queue the filter at the right time
-// add_action('pre_get_posts', 'exclude_protected_action');
-
-// add_filter('post_password_expires', 'set_post_password_expires');
+add_filter("get_pages", "ep_exclude_password_protected_pages", 10, 2);
 
 add_action( 'wp_enqueue_scripts', 'wpb_add_google_fonts' );
-
-// function my_favicon() { ?&amp;amp;amp;gt;
-// &amp;amp;amp;lt;link rel=&amp;amp;amp;quot;shortcut icon&amp;amp;amp;quot; href=&amp;amp;amp;quot;yourimagepathgoeshere&amp;amp;amp;quot; &amp;amp;amp;gt;
-// &amp;amp;amp;lt;?php }
-//
-// add_action('wp_head', 'my_favicon');
 
 add_action('wp_enqueue_scripts', 'hslSystems_scripts');
 
