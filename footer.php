8d82f1669c497261b1b139dc95297be281389a0b
mlkwok <mkwok@uw.edu>
2018-07-25 12:50:43 -0700

    add functioning logout method to templates

--- footer.php
+++ footer.php
@@ -1,15 +1,11 @@
 </div><!-- /.container -->
 <footer class="blog-footer">
 
-  <!-- <a href="https://www.facebook.com/uwhsl"><img src="<?php bloginfo('template_directory'); ?>/img/facebook.png"/></a>
-  <a href="https://twitter.com/uwhsl"><img src="<?php bloginfo('template_directory'); ?>/img/twitter.png"/></a> -->
-
   <div class="footer-link"><a href="/sitemap/">Sitemap</a></div>
   <div class="footer-link"><a href="mailto:syshelp@hsl.washington.edu">Contact</a></div>
   <div class="footer-link"><a href="/home/systems/meet-the-systems-staff/">About</a></div>
 
   <div class="footer-divider"></div>
-
 
   <div class="footer-link">© 2018 <a href="https://hsl.uw.edu/">University of Washington Health Sciences Library</a></div>
 
